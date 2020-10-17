9eb033af1ef5d86d6ac7f684db12fab52cac14cf
tvercauteren <tvercauteren@barclab.com>
2020-10-17 11:38:12 +0200

    add base ui to project

--- BWAdminUi/Shared/WeatherForecast.cs
+++ BWAdminUi/Shared/WeatherForecast.cs
@@ -1,6 +1,8 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace BWAdmin2._0
+namespace BWAdminUi.Shared
 {
     public class WeatherForecast
     {
@@ -8,8 +10,8 @@ namespace BWAdmin2._0
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
